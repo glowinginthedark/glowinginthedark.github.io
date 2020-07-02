beba75d4591d3da2f768d7aea2d7fecad1f7b53d
Bilal <solidhydrogenblock@gmail.com>
2020-07-01 19:37:11 -0700

    design update

--- main.js
+++ main.js
@@ -15,6 +15,16 @@ window.onload = () => {
 	let nav_menu_button = document.getElementById('nav_menu_button');
 	let header = document.getElementsByTagName('header')[0];
 	let nav = document.getElementsByTagName('nav')[0];
+	let html = document.getElementsByTagName('html')[0];
+	let body = document.getElementsByTagName('body')[0];
+	const aspectRatio = 1280.0/960.0;
+	
+	const updateBG = () => html.style.backgroundSize = 
+		window.innerWidth / window.innerHeight < aspectRatio ? 'auto 100%' : '100% auto';
+		
+	updateBG();
+	
+	window.addEventListener('resize', updateBG);
 
 	nav_menu_button.addEventListener('click', () => {
 
@@ -45,6 +55,22 @@ window.onload = () => {
 			nav.style.display = 'none';
 		}
 
+	});
+	
+	html.addEventListener('mousemove', (e) => {
+		const gradient = 'linear-gradient(rgba(0, 0, 0, 0.45), rgba(40, 4, 73, 0.45)), ';
+		const isInBody = isDescendant(body, e.target);
+		
+		html.style.background = isInBody ? gradient + 'url("./images/bg.jpg")' : gradient + 'url("./images/bg3.jpg")';
+		if (window.innerWidth / window.innerHeight < aspectRatio) {
+			html.style.backgroundSize = isInBody ? 'auto 101%' : 'auto 100%';
+		} else {
+			html.style.backgroundSize = isInBody ? '101% auto' : '100% auto';
+		}
+		html.style.backgroundRepeat = 'no-repeat';
+		html.style.backgroundPosition = 'center';
+		html.style.backgroundAttachment = 'fixed';
+		html.style.backgroundColor = 'black';
 	});
 }
 
